db6d3827d3275568037b096d2f9a263cb188b103
ryanak <ryank1985@live.com>
2013-11-26 01:45:22 -0800

    Cleaning things up, making sure clear cart option is not available when invalid entries are submitted.

--- order.php
+++ order.php
@@ -6,29 +6,29 @@
 		$_SESSION['orderedlist'] = $_POST['ordered'];
 	}
 
-    if((isset($_POST['quantity'])))
-    {
-         $quantity = $_POST['quantity'];
-    }
+	
+	if((isset($_POST['quantity'])))    {
+		$quantity = $_POST['quantity'];
+	} else    {
+		$quantity = $_SESSION['quantity'];
+	}
 
-    else
-    {
-        $quantity = $_SESSION['quantity'];
-    }
-   
+	
 	if(!empty($_SESSION['orderedlist'])){
 		foreach ($_SESSION['orderedlist'] as $pId){
-            if (is_numeric($quantity[$pId]) && $quantity[$pId] > 0) {
-			$price[] = number_format(($products[$pId]['price'] * $quantity[$pId]), 2);
-			$tax =  number_format((array_sum($price) * 0.0775),2);
-			$final = number_format($tax + array_sum($price),2);
-			$items[] = " " . $products[$pId]['name'] . " - " . " Cost: " . "$" . $products[$pId]['price'] . " - " . " Quantity: " . $quantity[$pId] . "<br>";
-            $_SESSION['quantity'][$pId] = $quantity[$pId];
+			
+			if (is_numeric($quantity[$pId]) && $quantity[$pId] > 0) {
+				$price[] = number_format(($products[$pId]['price'] * $quantity[$pId]), 2);
+				$tax =  number_format((array_sum($price) * 0.0775),2);
+				$final = number_format($tax + array_sum($price),2);
+				$items[] = " " . $products[$pId]['name'] . " - " . " Cost: " . "$" . $products[$pId]['price'] . " - " . " Quantity: " . $quantity[$pId] . "<br>";
+				$_SESSION['quantity'][$pId] = $quantity[$pId];
+			} else {
+				$not_numeric_value = TRUE;
+			}
+
 		}
-        else {
-        $not_numeric_value = TRUE;
-         }
-       }
+
 		$_SESSION['price'] = $price;
 		$_SESSION['tax'] = $tax;
 		$_SESSION['final'] = $final;
@@ -47,17 +47,15 @@
 <h1 class='left'>BBQ Online Store</h1>
 <div class="clear"></div>
 <?php
-	if ($not_numeric_value == TRUE)
-    {
-        echo "<div><p class='left'>Your Order: </p><p class='right'><a href='index.php'>Return to Order Page</a></p>
-    <div class='clear'></div>";
+	
+	if ($not_numeric_value == TRUE)    {
+		echo "<div><p class='left'>Your Order: </p><p class='right'><a href='index.php'>Return to Order Page</a></p><div class='clear'></div>";
 		echo "One of your quantities is invalid. Please return to the order page and input proper numeric quantities.";
 		echo "</div>";
-    }
-    elseif (!empty($_SESSION['items'])){
-		echo "<div>
-<p class='left'>Your Order: </p><p class='right'><a href='index.php'>Return to Order Page</a></p>
-<div class='clear'></div>";
+	}
+
+	elseif (!empty($_SESSION['items']))    {
+		echo "<div><p class='left'>Your Order: </p><p class='right'><a href='index.php'>Return to Order Page</a></p><div class='clear'></div>";
 		echo "<ul><b>";
 		echo implode("<li></li>", $_SESSION['items']);
 		echo "</b></ul>";
@@ -68,19 +66,14 @@
 		echo "<li><br><a href='checkout.php'>Continue with Checkout</a><li>";
 		echo "</ul>";
 	} else {
-		echo "<div><p class='left'>Your Order: </p><p class='right'><a href='index.php'>Return to Order Page</a></p>
-    <div class='clear'></div>";
+		echo "<div><p class='left'>Your Order: </p><p class='right'><a href='index.php'>Return to Order Page</a></p><div class='clear'></div>";
 		echo "Your cart is currently empty.";
 		echo "</div>";
 	}
 
-	?>
-<br /><br />
-<?php 
 	
-	if(!empty($_SESSION['items']))
-	{
-		echo "<a href='clearcart.php'>Clear Cart</a>";
+	if(!empty($_SESSION['items']) || $not_numeric_value == !TRUE){
+		echo "<br><br><a href='clearcart.php'>Clear Cart</a>";
 	}
 
 	?>
